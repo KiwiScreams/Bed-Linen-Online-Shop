70c333d8522409a67ee714473a7b234d69e2bc3e
KiwiScreams <screamskiwi3@gmail.com>
2024-09-13 15:45:10 +0400

    header

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,10 +1,42 @@
 import "./Header.css";
+import { Link } from "react-router-dom";
+import cartIcon from "../../../assets/img/Shopping.svg";
+import userIcon from "../../../assets/img/profile.svg";
+import languageIcon from "../../../assets/img/language.svg";
 const Header = () => {
-    return ( 
-        <>
-        h
-        </>
-     );
-}
- 
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link>Home</Link>
+            </li>
+            <li>
+              <Link>About</Link>
+            </li>
+            <li>
+              <Link>Payment</Link>
+            </li>
+            <li>
+              <Link>Catalog</Link>
+            </li>
+            <li>
+              <Link>Shop</Link>
+            </li>
+            <li>
+              <Link>Blog</Link>
+            </li>
+          </ul>
+          <ul>
+            <li><img src={cartIcon} alt="" /></li>
+            <li><img src={userIcon} alt="" /></li>
+            <li><img src={languageIcon} alt="" /></li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
 export default Header;
