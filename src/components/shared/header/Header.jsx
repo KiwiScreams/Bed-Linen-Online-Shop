9ae105c9ef99b3f4130a7b2f02e3047f274ccfbd
KiwiScreams <screamskiwi3@gmail.com>
2024-09-13 16:22:30 +0400

    responsive header

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -4,11 +4,13 @@ import cartIcon from "../../../assets/img/Shopping.svg";
 import userIcon from "../../../assets/img/profile.svg";
 import languageIcon from "../../../assets/img/language.svg";
 import activeIcon from "../../../assets/img/game-icons_feather.png";
+import { useState, useEffect } from "react";
 const Header = () => {
   const location = useLocation();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
-      <header>
+      <header className="desktop">
         <nav>
           <ul>
             <li>
@@ -30,7 +32,9 @@ const Header = () => {
             <li>
               <Link
                 to="/payment"
-                className={location.pathname === "/payment" ? "active-link" : ""}
+                className={
+                  location.pathname === "/payment" ? "active-link" : ""
+                }
               >
                 Payment
               </Link>
@@ -38,7 +42,78 @@ const Header = () => {
             <li>
               <Link
                 to="/catalog"
-                className={location.pathname === "/catalog" ? "active-link" : ""}
+                className={
+                  location.pathname === "/catalog" ? "active-link" : ""
+                }
+              >
+                Catalog
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/shop"
+                className={location.pathname === "/shop" ? "active-link" : ""}
+              >
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/blog"
+                className={location.pathname === "/blog" ? "active-link" : ""}
+              >
+                Blog
+              </Link>
+            </li>
+          </ul>
+          <ul>
+            <li className="pointer">
+              <img src={cartIcon} alt="" />
+            </li>
+            <li className="pointer">
+              <img src={userIcon} alt="" />
+            </li>
+            <li className="pointer">
+              <img src={languageIcon} alt="" />
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <header className="mobile">
+        <nav>
+          <ul>
+            <li>
+              <Link
+                to="/"
+                className={location.pathname === "/" ? "active-link" : ""}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                className={location.pathname === "/about" ? "active-link" : ""}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/payment"
+                className={
+                  location.pathname === "/payment" ? "active-link" : ""
+                }
+              >
+                Payment
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/catalog"
+                className={
+                  location.pathname === "/catalog" ? "active-link" : ""
+                }
               >
                 Catalog
               </Link>
