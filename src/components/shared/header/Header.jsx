11f2a09f239cc0c0a29f045c70c918831ce7cdb7
KiwiScreams <screamskiwi3@gmail.com>
2024-09-13 16:32:12 +0400

    nav open closed

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -82,8 +82,9 @@ const Header = () => {
           </ul>
         </nav>
       </header>
+
       <header className="mobile">
-        <nav>
+        <nav className={`${isMenuOpen ? "nav-open" : "nav-closed"}`}>
           <div
             className={`bar-container ${isMenuOpen ? "change" : ""}`}
             onClick={handleMenuToggle}
@@ -92,6 +93,60 @@ const Header = () => {
             <div className="bar2"></div>
             <div className="bar3"></div>
           </div>
+          <ul className={`${isMenuOpen ? "nav-open" : "nav-closed"}`}>
+            <li>
+              <Link
+                to="/"
+                className={location.pathname === "/" ? "active-link" : ""}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/about"
+                className={location.pathname === "/about" ? "active-link" : ""}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/payment"
+                className={
+                  location.pathname === "/payment" ? "active-link" : ""
+                }
+              >
+                Payment
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/catalog"
+                className={
+                  location.pathname === "/catalog" ? "active-link" : ""
+                }
+              >
+                Catalog
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/shop"
+                className={location.pathname === "/shop" ? "active-link" : ""}
+              >
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/blog"
+                className={location.pathname === "/blog" ? "active-link" : ""}
+              >
+                Blog
+              </Link>
+            </li>
+          </ul>
           <ul>
             <li className="pointer">
               <img src={cartIcon} alt="" />
