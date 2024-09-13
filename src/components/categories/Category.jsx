2b8d2f319403517156c17942545a2f0932469e4d
KiwiScreams <screamskiwi3@gmail.com>
2024-09-13 18:30:04 +0400

    create categories

--- src/components/categories/Category.jsx
+++ src/components/categories/Category.jsx
@@ -1,8 +1,36 @@
 import "./Category.css";
-const Category = () => {  
-    return ( 
-        <></>
-     );
-}
- 
+import img1 from "../../assets/img/Group.png";
+import img2 from "../../assets/img/Group 1.png";
+import img3 from "../../assets/img/Group (1).png";
+import img4 from "../../assets/img/game-icons_feather.png";
+const Category = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Healthy Sleep",
+      image: img1,
+    },
+    {
+      id: 2,
+      title: "100%Cotton",
+      image: img2,
+    },
+    {
+      id: 3,
+      title: "Best Tailoring",
+      image: img3,
+    },
+    {
+      id: 4,
+      title: "Comfort",
+      image: img4,
+    },
+  ];
+  return (
+    <>
+      <section className="categories-section"></section>
+    </>
+  );
+};
+
 export default Category;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
+import Category from "../../components/categories/Category";
 import Hero from "../../components/hero/Hero";
 import "./Home.css";
 const Home = () => {
   return (
     <>
       <Hero />
+      <Category />
     </>
   );
 };
