2e2bbba0673a510b367541a666f24929ba1738d5
KiwiScreams <screamskiwi3@gmail.com>
2024-09-13 18:14:43 +0400

    hero image

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,29 @@ import arrowIcon from "../../assets/img/arrow.png";
 import circleIcon from "../../assets/img/cyrcle.png";
 import heroImage from "../../assets/img/Hero-pic.png";
 import feathersImage from "../../assets/img/Feather-bg.png";
+import { useRef, useEffect } from "react";
+
 const Hero = () => {
+  const feathersImageRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      const mouseX = event.clientX;
+      const mouseY = event.clientY;
+
+      if (feathersImageRef.current) {
+        const rotation = `rotate(${(mouseX / window.innerWidth) * 10}deg)`;
+        feathersImageRef.current.style.transform = rotation;
+      }
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [feathersImageRef]);
+
   return (
     <>
       <section className="hero-component">
@@ -17,7 +39,7 @@ const Hero = () => {
       </section>
       <div className="hero-images">
         <img src={heroImage} alt="" className="hero-image" />
-        <img src={feathersImage} alt="" />
+        <img ref={feathersImageRef} src={feathersImage} alt="" />
       </div>
     </>
   );
