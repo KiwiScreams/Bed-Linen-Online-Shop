bdd3cbb8cc2964b6872a99e62cb1161fc7ebbe76
KiwiScreams <screamskiwi3@gmail.com>
2024-09-13 17:31:35 +0400

    hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,20 @@
 import "./Hero.css";
+import arrowIcon from "../../assets/img/arrow.png";
+import circleIcon from "../../assets/img/cyrcle.png";
 const Hero = () => {
-    return ( 
-        <>
-        Hero
-        </>
-     );
-}
- 
+  return (
+    <>
+      <section className="hero-component">
+        <h1>Dream in comfort</h1>
+        <p>Creating your perfect sleep sanctuary with us</p>
+        <button>
+            <img src={arrowIcon} alt="" />
+            <img src={circleIcon} alt="" />
+            shop now
+        </button>
+      </section>
+    </>
+  );
+};
+
 export default Hero;
