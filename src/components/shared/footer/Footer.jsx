91924aa3f59af803be3eeb387d3a39da98c4cdf7
KiwiScreams <screamskiwi3@gmail.com>
2024-09-14 21:11:08 +0400

    footer

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -1,9 +1,39 @@
 import "./Footer.css";
+
 const Footer = () => {
   return (
     <>
       <footer>
         <div className="footer-img"></div>
+        <div>
+          <ul>
+            <li>About</li>
+            <li>Our Story</li>
+            <li>Our Impact</li>
+            <li>FAQ</li>
+          </ul>
+          <ul>
+            <li>Customer Recourses</li>
+            <li>E-Catalog</li>
+            <li>Request Catalog</li>
+            <li>Support</li>
+          </ul>
+          <ul>
+            <li>Services</li>
+            <li>Delivery & Shipping</li>
+            <li>Returns</li>
+            <li>Guaranlee</li>
+          </ul>
+          <ul>
+            <li>
+              <img src={locationIcon} alt="" />
+              Services
+            </li>
+            <li>Delivery & Shipping</li>
+            <li>Returns</li>
+            <li>Guaranlee</li>
+          </ul>
+        </div>
       </footer>
     </>
   );
