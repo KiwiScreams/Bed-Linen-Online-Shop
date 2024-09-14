a7fe7278a55ccba69216d753e15c81da52376b99
KiwiScreams <screamskiwi3@gmail.com>
2024-09-14 19:42:50 +0400

    sale styles

--- src/components/sale/Sale.jsx
+++ src/components/sale/Sale.jsx
@@ -5,18 +5,20 @@ const Sale = () => {
   return (
     <>
       <section className="sale-section">
-        <h2>SAVE UP TO 70% OFF</h2>
-        <p>
-          Our pillows are the perfect way to improve your sleep quality, and now
-          there's even more incentive to upgrade with our 70% off offer! Get
-          cozy with a premium pillow that is soft, supportive, and durable
-          enough to last for years
-        </p>
-        <button>
-          <img src={arrowIcon} alt="" />
-          <img src={circleIcon} alt="" />
-          <span>Shop Now</span>
-        </button>
+        <div>
+          <h2>SAVE UP TO 70% OFF</h2>
+          <p>
+            Our pillows are the perfect way to improve your sleep quality, and
+            now there's even more incentive to upgrade with our 70% off offer!
+            Get cozy with a premium pillow that is soft, supportive, and durable
+            enough to last for years
+          </p>
+          <button>
+            <img src={arrowIcon} alt="" />
+            <img src={circleIcon} alt="" />
+            <span>Shop Now</span>
+          </button>
+        </div>
       </section>
     </>
   );
