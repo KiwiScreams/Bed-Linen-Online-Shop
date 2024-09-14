b8c39bb783cbcae8becc8c0eb123f2af8d994590
KiwiScreams <screamskiwi3@gmail.com>
2024-09-14 18:52:56 +0400

    about section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Infobox from "../../components/infobox/Infobox";
 import Hero from "../../components/hero/Hero";
 import "./Home.css";
+import About from "../../components/about us/About";
 const Home = () => {
   return (
     <>
       <Hero />
       <Infobox />
+      <About />
     </>
   );
 };
