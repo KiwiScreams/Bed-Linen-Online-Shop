1d2a9f7dea100fc0fc42799e2a47a6acf8dce0d4
KiwiScreams <screamskiwi3@gmail.com>
2024-09-14 19:34:47 +0400

    sale component

--- src/components/sale/Sale.jsx
+++ src/components/sale/Sale.jsx
@@ -1,8 +1,25 @@
 import "./Sale.css";
+import arrowIcon from "../../assets/img/arrow.png";
+import circleIcon from "../../assets/img/cyrcle.png";
 const Sale = () => {
-    return ( 
-        <></>
-     );
-}
- 
+  return (
+    <>
+      <section className="sale-section">
+        <h2>SAVE UP TO 70% OFF</h2>
+        <p>
+          Our pillows are the perfect way to improve your sleep quality, and now
+          there's even more incentive to upgrade with our 70% off offer! Get
+          cozy with a premium pillow that is soft, supportive, and durable
+          enough to last for years
+        </p>
+        <button>
+          <img src={arrowIcon} alt="" />
+          <img src={circleIcon} alt="" />
+          <span>Shop Now</span>
+        </button>
+      </section>
+    </>
+  );
+};
+
 export default Sale;
