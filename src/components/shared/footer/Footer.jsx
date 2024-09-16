b2a6e7f83ecd4d43983ee6777baf10ecdbea4a53
KiwiScreams <screamskiwi3@gmail.com>
2024-09-16 18:24:10 +0400

    footer

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -3,7 +3,11 @@ import PhoneIcon from "../../../assets/img/Vector (1).svg";
 import linkedinIcon from "../../../assets/img/Vector (2).svg";
 import pinterestIcon from "../../../assets/img/Vector (3).svg";
 import instagramIcon from "../../../assets/img/Vector (4).svg";
-import locationIcon from "../../../assets/img/Vector (6).svg";
+// import locationIcon from "../../../assets/img/Vector (6).svg";
+import arrowLeft from "../../../assets/img/Vector (6).svg";
+import arrowRight from "../../../assets/img/Vector (5).svg";
+import emailIcon from "../../../assets/img/Vector.svg";
+import locationIcon from "../../../assets/img/location.svg";
 const Footer = () => {
   return (
     <>
@@ -29,14 +33,33 @@ const Footer = () => {
             <li>Guaranlee</li>
           </ul>
           <ul>
-            <li>
-              <img src={locationIcon} alt="" />
-              Services
-            </li>
+            <li>Services</li>
             <li>Delivery & Shipping</li>
             <li>Returns</li>
             <li>Guaranlee</li>
           </ul>
+          <div>
+            <ul>
+              <li>
+                <img src={locationIcon} alt="" />
+                12 jhon Avenue #2, new York
+              </li>
+              <li>
+                <img src={emailIcon} alt="" />
+                sleepy@shop.com
+              </li>
+              <li>
+                <img src={PhoneIcon} alt="" />
+                +1-222-34-sleep
+              </li>
+            </ul>
+            <div>
+              <span>Social Media: </span>
+              <img src={linkedinIcon} alt="" />
+              <img src={pinterestIcon} alt="" />
+              <img src={instagramIcon} alt="" />
+            </div>
+          </div>
         </div>
       </footer>
     </>
